f5247c9ed4cb03ddf2d7f768ed34c69e92ea39d6
柳鑫鹏 <>
2020-11-25 16:08:18 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,18 @@ Route::get('/', function () {
 
 Route::get('/blog', 'BlogController@index')->name('blog.home');
 Route::get('/blog/{slug}', 'BlogController@showPost')->name('blog.detail');
+
+// 后台路由
+Route::get('/admin', function () {
+    return redirect('/admin/post');
+});
+Route::middleware('auth')->namespace('Admin')->group(function () {
+    Route::resource('admin/post', 'PostController');
+    Route::resource('admin/tag', 'TagController');
+    Route::get('admin/upload', 'UploadController@index');
+});
+
+// 登录退出
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/login', 'Auth\LoginController@login');
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
